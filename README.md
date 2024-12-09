2f5db1178a00b03d05181e3f817b65a67755dc54
MaTor97 <mttortora97@gmail.com>
2024-12-09 14:31:40 +0100

    Added components

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ In this project, we will learn the basic of react through the construction of a 
     - select your project name
     - choose your framework (react)
     - choose the variant (javascript)
+
+2. Create all your components
+
+    - an input field to type new todos
+    - a submit button
+    - a list of todos (just add 2-3 todos)
+    - each todo has a checkbox next to it
--- react_to_do_list/src/App.jsx
+++ react_to_do_list/src/App.jsx
@@ -3,33 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let todos = ['Learn React', 'Be awesome!', 'Practice Javascript']
 
+const MyTodo = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <h1>My Todo App</h1>
+      <Header />
+      <Todos todos={todos}/>
+    </main>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Type a new todo" />
+      <button>Add Todo</button>
+    </div>
+  );
+};
+
+const Todos = () => {
+  return (
+    <div>
+      <h2>Todos</h2>
+      <Element  todos={todos}/>
+    </div>
+  );
+};
+
+const Element = () => {
+  return (
+    <div>
+      {todos.map((todo, index) => (
+        <div key={index}>
+          <input type="checkbox" id={`todo-${index}`} />
+          <label htmlFor={`todo-${index}`}>{todo}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
+function App() {
+  return(
+    <div>
+      <MyTodo />
+    </div>
+  );
+};
 
 export default App
